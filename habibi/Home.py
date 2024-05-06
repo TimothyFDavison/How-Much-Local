e5f861e79e9df6859c11569c7a53a16a348e7c75
Tim <timothyfdavison@gmail.com>
2024-05-06 12:36:16 -0500

    Adding progress tracking v1

--- habibi/Home.py
+++ habibi/Home.py
@@ -1,25 +1,31 @@
 import streamlit as st
 
+import altair as alt
 import pandas as pd
 import random
+from time import time
 
-# Global variables
-medications_list = [
-    "0.25% Plain Lidocaine",
-    "0.25% Plain Bupivacaine",
-    "0.25% Lidocaine with Epinephrine",
-    "0.5% Plain Bupivacaine",
-    "0.5% Bupivacaine with Epinephrine",
-    "1% Plain Lidocaine",
-    "1% Lidocaine with Epinephrine",
-    "2% Plain Lidocaine",
-    "2% Lidocaine with Epinephrine"
+
+# Control variables
+anesthetics_list = [
+        "0.25% Plain Lidocaine",
+        "0.25% Plain Bupivacaine",
+        "0.25% Lidocaine with Epinephrine",
+        "0.5% Plain Bupivacaine",
+        "0.5% Bupivacaine with Epinephrine",
+        "1% Plain Lidocaine",
+        "1% Lidocaine with Epinephrine",
+        "2% Plain Lidocaine",
+        "2% Lidocaine with Epinephrine"
 ]
-
+if "anesthetics_options" not in st.session_state:
+    st.session_state.anesthetics_options = anesthetics_list
 if "input_weight" not in st.session_state:
     st.session_state.input_weight = random.randint(10, 80)
 if "input_anesthetic" not in st.session_state:
-    st.session_state.input_anesthetic = random.choice(medications_list)
+    st.session_state.input_anesthetic = random.choice(st.session_state.anesthetics_options)
+
+# Display variables
 if "text" not in st.session_state:
     st.session_state.text = ""
 if "show_formula" not in st.session_state:
@@ -27,25 +33,30 @@ if "show_formula" not in st.session_state:
 if "show_table" not in st.session_state:
     st.session_state.show_table = False
 
+# User tracking
+if "user_differentials" not in st.session_state:
+    st.session_state.user_differentials = []
+if "user_times" not in st.session_state:
+    st.session_state.user_times = []
+
 
 def clear_fields():
     st.session_state["text"] = ""
 
 
 @st.cache_data
-def get_data(shuffle=None):
-    shuffle = None
+def get_data():
+    """
+    Will persist per session.
+    """
     st.session_state["input_weight"] = random.randint(10, 80)
-    st.session_state["input_anesthetic"] = random.choice(medications_list)
+    st.session_state["input_anesthetic"] = random.choice(st.session_state.anesthetics_options)
     return
 
 
 def calculate_safe_dose(weight, input):
     """
-
-    @param weight:
-    @param input:
-    @return:
+    Calculate the dosage. To-do, move the list to a config file.
     """
     safe_dose = {
         "0.25% Plain Lidocaine": 4,
@@ -60,7 +71,7 @@ def calculate_safe_dose(weight, input):
     }
     percent = float(input.split("%")[0])
     answer = weight * safe_dose[input] * (.1 / percent)  # 1mL/%*10mg
-    answer = round(answer, 2)
+    answer = round(answer, 4)
     return answer, safe_dose[input], percent
 
 
@@ -68,31 +79,81 @@ if __name__ == "__main__":
 
     # Sidebar - Settings
     st.sidebar.markdown("# Settings")
-    configure = st.sidebar.checkbox("Show Settings")
-    if configure:
-        list = st.sidebar.multiselect(
-            label="Anesthetics",
-            options=medications_list,
-            default=medications_list
+    show_settings = st.sidebar.checkbox("Update Settings")
+    if show_settings:
+        st.markdown("""
+            <style>
+                .stMultiSelect [data-baseweb=select] span{
+                    max-width: 250px;
+                    font-size: 0.6rem;
+                }
+            </style>
+            """, unsafe_allow_html=True)
+        anesthetics_list  = st.sidebar.multiselect(
+            label="Anesthetic Options",
+            options=anesthetics_list,
+            default=st.session_state.anesthetics_options,
         )
+        save_settings = st.sidebar.button("Save")
+        if save_settings:
+            st.session_state.anesthetics_options = anesthetics_list
+            st.sidebar.success("Updated!")
+
+    # Sidebar - Progress
+    show_progress = st.sidebar.checkbox("Show Progress")
+    if show_progress:
+        if not st.session_state.user_times:
+            st.sidebar.warning('Progress tracking requires one or more submitted answers.')
+            st.stop()
+        st.sidebar.markdown("Tracking response time and accuracy. Data resets on a page refresh.")
+        problem_index = [x for x in range(1, len(st.session_state.user_times)+1)]
+        user_times = pd.DataFrame(
+            {
+                "Time (s)": st.session_state.user_times,
+                "Problem": problem_index
+            }
+        )
+        times_chart = alt.Chart(
+            user_times,
+            title=alt.Title(
+                "User Response Time"
+            )
+        ).mark_line().encode(
+            x=alt.X('Problem', axis=alt.Axis(values=problem_index, labelOverlap="greedy", grid=False)),
+            y=alt.Y('Time (s)', axis=alt.Axis(tickCount=5, labelOverlap="greedy", grid=False))
+        )
+        st.sidebar.altair_chart(times_chart, use_container_width=True)
+        print(st.session_state.user_differentials)
+        user_differentials = pd.DataFrame(
+            {
+                "Differential (mL)": st.session_state.user_differentials,
+                "Problem": problem_index
+            }
+        )
+        differentials_chart = alt.Chart(
+            user_differentials,
+            title=alt.Title(
+                "Differential"
+            )
+        ).mark_line().encode(
+            x=alt.X('Problem', axis=alt.Axis(values=problem_index, labelOverlap="greedy", grid=False)),
+            y=alt.Y('Differential (mL)', axis=alt.Axis(tickCount=5, labelOverlap="greedy", grid=False))
+        )
+        st.sidebar.altair_chart(differentials_chart, use_container_width=True)
 
     # Sidebar - Resources
     st.sidebar.markdown("# References")
     st.sidebar.markdown("## **Local Anesthetic Characteristics**")
-    st.sidebar.write("Becker DE, Reed KL. Local anesthetics: review of pharmacological considerations. Anesth Prog. 2012 Summer;59(2):90-101; quiz 102-3. doi: 10.2344/0003-3006-59.2.90. PMID: 22822998; PMCID: PMC3403589.")
-
+    st.sidebar.markdown("Becker DE, Reed KL. Local anesthetics: review of pharmacological considerations. Anesth Prog. 2012 Summer;59(2):90-101; quiz 102-3. doi: 10.2344/0003-3006-59.2.90. PMID: 22822998; PMCID: PMC3403589.")
     st.sidebar.markdown("## **Easy Calculation**")
     st.sidebar.markdown("Ahmed OA. Local anaesthetics for surgeons: A simple method to calculate the safe volume of local anaesthetic for infiltration during surgical procedures. J Plast Reconstr Aesthet Surg. 2023 Aug;83:479-480. doi: 10.1016/j.bjps.2023.05.034. Epub 2023 May 19. PMID: 37356407.")
-
     st.sidebar.markdown("## **Injection Technique**")
     st.sidebar.markdown("Joukhadar N, Lalonde D. How to Minimize the Pain of Local Anesthetic Injection for Wide Awake Surgery. Plast Reconstr Surg Glob Open. 2021 Aug 4;9(8):e3730. doi: 10.1097/GOX.0000000000003730. PMID: 34367856; PMCID: PMC8337068")
     st.sidebar.markdown("Lalonde D, Wong A. Local Anesthetics: What's New in Minimal Pain Injection and Best Evidence in Pain Control. Plast Reconstr Surg. 2014 Oct;134(4 Suppl 2):40S-49S. doi: 10.1097/PRS.0000000000000679. PMID: 25255006.")
     st.sidebar.markdown("Gottlieb M, Penington A, Schraft E. Digital Nerve Blocks: A Comprehensive Review of Techniques. J Emerg Med. 2022 Oct;63(4):533-540. doi: 10.1016/j.jemermed.2022.07.002. Epub 2022 Oct 11. PMID: 36229322.")
     st.sidebar.markdown("Moskovitz JB, Sabatino F. Regional nerve blocks of the face. Emerg Med Clin North Am. 2013 May;31(2):517-27. doi: 10.1016/j.emc.2013.01.003. Epub 2013 Feb 18. PMID: 23601486.")
-
     st.sidebar.markdown("## **Toxicity Signs and Treatment**")
     st.sidebar.markdown("Gitman M, Fettiplace MR, Weinberg GL, Neal JM, Barrington MJ. Local Anesthetic Systemic Toxicity: A Narrative Literature Review and Clinical Update on Prevention, Diagnosis, and Management. Plast Reconstr Surg. 2019 Sep;144(3):783-795. doi: 10.1097/PRS.0000000000005989. PMID: 31461049.")
-
     st.sidebar.markdown("## **Education Theory**")
     st.sidebar.markdown("Gitman M, Fettiplace MR, Weinberg GL, Neal JM, Barrington MJ. Local Anesthetic Systemic Toxicity: A Narrative Literature Review and Clinical Update on Prevention, Diagnosis, and Management. Plast Reconstr Surg. 2019 Sep;144(3):783-795. doi: 10.1097/PRS.0000000000005989. PMID: 31461049.")
 
@@ -105,6 +166,7 @@ if __name__ == "__main__":
         st.session_state.input_anesthetic
     )
 
+    start_time = time()
     input_answer = st.text_input(
         label="Answer (mL)",
         value="",
@@ -122,14 +184,20 @@ if __name__ == "__main__":
             st.warning("Please input a number in decimal form.")
             st.stop()
 
-        differential = round(abs(answer - input_answer), 2)
+        differential = round(abs(answer - input_answer), 4)
+        rounded_differential = round(differential, 2)
+
+        end_time = time()
+        total_time = round(end_time - start_time, 4)
+        st.session_state.user_times.append(total_time)
+        st.session_state.user_differentials.append(differential)
         st.success(f"{answer} mL")
         if differential < 5:
-            st.success(f"Off by: {differential} mL")
+            st.success(f"Off by: {rounded_differential} mL")
         elif differential < 10:
-            st.warning(f"Off by {differential} mL")
+            st.warning(f"Off by {rounded_differential} mL")
         else:
-            st.error(f"Off by: {differential} mL")
+            st.error(f"Off by {rounded_differential} mL")
 
     # Display variables
     show_formula = st.checkbox("Show Formula")
@@ -147,5 +215,9 @@ if __name__ == "__main__":
     if next:
         input_answer = None
         st.session_state["input_weight"] = random.randint(10, 80)
-        st.session_state["input_anesthetic"] = random.choice(medications_list)
+        try:
+            st.session_state.input_anesthetic = random.choice(st.session_state.anesthetics_options)
+        except:
+            st.warning("You must select at least one anesthetic in Settings.")
+            st.stop()
         st.rerun()
